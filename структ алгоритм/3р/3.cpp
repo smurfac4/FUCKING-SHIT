4a4b4185384837584814046e38ae12e338206969
Alex Vanslov <aleksvanslov@gmail.com>
2023-03-02 16:47:56 +0300

    ------

--- структ алгоритм/3р/3.cpp
+++ структ алгоритм/3р/3.cpp
@@ -98,16 +98,20 @@ void mergeSort(vector<int> &a, size_t start, size_t end)
 int main()
 {
     vector<int> v;
-    for (int i = 0; i < 20; ++i)
+    for (int i = 0; i < 20; ++i){
         v.push_back(i);
-    for (size_t i = 0; i < v.size(); ++i)
+    }
+    for (size_t i = 0; i < v.size(); ++i){
         swap(v[i], v[rand() % (v.size() - i) + i]);
-    for (auto i: v)
+    }
+    for (auto i: v){
         cout << i << " ";
+    }
     cout << endl;
     mergeSort(v, 0, v.size());
-    for (auto i: v)
+    for (auto i: v){
         cout << i << " ";
+    }
     cout << endl;
 }
 
