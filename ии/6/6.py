44b615cfc90f9f97da96d8806e93033ecc738ef3
Alex Vanslov <aleksvanslov@gmail.com>
2023-03-04 19:36:41 +0300

    --

--- ии/6/6.py
+++ ии/6/6.py
@@ -7,7 +7,6 @@ def qsumZ(Z):
 def exchangeScheme(oldX, oldY, sortedId): 
     X = [0 for i in range(4)] 
     Y = [0 for i in range(4)]
-    print(oldX)
     X[2] = oldX[sortedId[2]] 
     X[3] = oldX[sortedId[2]]
     X[0] = oldX[sortedId[0]]
@@ -20,9 +19,7 @@ def exchangeScheme(oldX, oldY, sortedId):
 
 
 def sorting(Z):
-    print(Z,1)
     sortedId = sorted(range(len(Z)), key = lambda k: Z[k])
-    print(sortedId,2)
     return sortedId
 
 
@@ -31,7 +28,7 @@ def evoStep(X, Y, Z):
     X = X[:]
     Y = Y[:]
     Z = Z[:]
-    X. pop(minId)
+    X.pop(minId)
     Y.pop(minId)
     Z.pop(minId)
     return X, Y, Z
@@ -41,18 +38,17 @@ def evoSteps(X, Y):
     for i in range(4): 
         arrZ = [qZ(x, Y[i]) for i, x in enumerate(X)]
         X,Y,Z = evoStep(X, Y, arrZ)
-        Z, Y = exchangeScheme(X, Y, sorting(Z))
+        X,Y = exchangeScheme(X, Y, sorting(Z))
         results.append([X, Y, qsumZ(arrZ), arrZ])
     return X, Y, results
 
 X = [-1,0,2,3]
 Y = [0,1,-2,2]
 results = evoSteps(X,Y)
-
 for i in range(len(results[2])):
-    print(results[2][i][2])
+    print(f'max_{i+1}: {results[2][i][2]}')
 
 qualityArrZ = [] 
 for i in range(len(results[2])):
     qualityArrZ += results[2][i][3]
-print(f'max Z: {max(qualityArrZ)}')
+print(f'max_Z: {max(qualityArrZ)}')
